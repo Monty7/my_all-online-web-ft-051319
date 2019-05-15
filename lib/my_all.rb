9dc74d1fec3303399f0d55b3b38771b6f21df02c
voluble-bug-0219 <montensia1@outlook.com>
2019-05-15 23:24:48 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -4,7 +4,12 @@ def my_all?(collection)
   i = 0
   block_return_values = []
   while i < collection.length 
+    block_return_values << yield(collection(i))
     i = i + 1 
   end
-  
+  if
+    block_return_values.include?(false)
 end
+
+my_all?([1,2,3]) {|i| i < 2}
+
